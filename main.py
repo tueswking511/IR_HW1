45ec44182d54e4cc7953b97e845ca7f07a5979b2
107598003 <tueswking511@gmail.com>
2019-03-15 14:33:41 +0800

    Hw1

--- main.py
+++ main.py
@@ -7,20 +7,33 @@ for record in f:
     if record['Warc-type'] == 'warcinfo':
         pass
     else:
+        print record
         temp = '\n'
         str = record.__str__()
         for i in range(24):
             if str != "":
                 npos = str.index(temp)
-                str = str[npos+1:]
+                str = str[npos + 1:]
         h = html2text.HTML2Text()
         count = count + 1
         try:
             output = h.handle(str.decode("utf-8"))
-            print count.__str__() + " Web finish"
+            # print count.__str__() + " Web finish"
+            # print type(output.__str__())
             # print output
+            # print output.__str__().split()
+            outputlist = output.__str__().split()
+            # print outputlist
+            # outputlist = filter(outputlist)
+            for i in outputlist:
+                # print 'Old:' + i.__str__()
+                j = i.__str__()
+                j = j.lower()
+                j = filter(lambda ch: ch in 'abcdefghijklmnopqrstuvwxyz', j)
+                if j != "":
+                    print j.__str__()
         except UnicodeDecodeError:
             output = h.handle(str.decode("latin-1"))
             print count.__str__() + " Web finish"
             # print output
-print count
+        break
